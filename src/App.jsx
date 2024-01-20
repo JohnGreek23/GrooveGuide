97b46a3766eecf36c508c9cb44a4d5fef326f3bb
Alex Papadakis <Lx.papad@gmail.com>
2024-01-20 09:36:54 +0000

    Adds some of the jsx needed for functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,100 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import { Form, Button } from 'react-bootstrap';
 
-import Navbar from './components/Navbar/navbar';
-import Jumbotron from './components/Jumbotron/jumbotron';
+function MusicApp() {
+  const [genre, setGenre] = useState('');
+  const [artist, setArtist] = useState('');
+  const [artists, setArtists] = useState([]);
+  const [events, setEvents] = useState([]);
 
+  const handleGenreChange = (e) => {
+    setGenre(e.target.value);
+  };
 
-// import all the components you need 
+  const handleArtistChange = (e) => {
+    setArtist(e.target.value);
+  };
 
-function App() {
-    return (
-        <div>
-            {/* place all components here so rendered */}
-            
-            <Navbar />
-            <Jumbotron />
-        </div>
-    )
+  const handleSubmit = (e) => {
+    e.preventDefault();
+     if (genre) {
+      fetchRecommendedArtists(genre);
+    }
+    if (artist) {
+      fetchRecommendedEvents(artist);
+    }
+  };
+
+  const fetchRecommendedEvents = (artist) => {
+    const apiKey = 'pKiE2zYyc7KexxDyMtquVIRjuChhCFzo';
+    const location = 'YOUR_USER_LOCATION';
+  
+    fetch(`https://app.ticketmaster.com/discovery/v2/events.json?keyword=${artist}&apikey=${apiKey}&location=${location}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setEvents(data?._embedded?.events || []);
+      })
+      .catch((error) => {
+        console.error('Error fetching events:', error);
+      });
+  };
+
+  useEffect(() => {
+    
+  }, []);
+
+  return (
+    <div className="music-app">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="genre">
+          <Form.Label>Pick a genre</Form.Label>
+          <Form.Control as="select" value={genre} onChange={handleGenreChange}>
+            <option value="">Select Genre</option>
+            <option value="rock">Rock</option>
+            <option value="pop">Pop</option>
+            <option value="hip-hop">Hip Hop</option>
+            <option value="country">Country</option>
+          </Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="artist">
+          <Form.Label>Or search for an artist</Form.Label>
+          <Form.Control type="text" value={artist} onChange={handleArtistChange} />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+
+      <Tabs>
+        <TabList>
+          <Tab>Recommended Artists</Tab>
+          <Tab>Recommended Events</Tab>
+        </TabList>
+
+        <TabPanel>
+          {artists.map((artist) => (
+            <div key={artist.id}>
+              <h3>{artist.name}</h3>
+              {/* other artist information */}
+            </div>
+          ))}
+        </TabPanel>
+
+        <TabPanel>
+          {events.map((event) => (
+            <div key={event.id}>
+              <h3>{event.name}</h3>
+              {/* other event information */}
+            </div>
+          ))}
+        </TabPanel>
+      </Tabs>
+    </div>
+  );
 }
 
-export default App;
+export default MusicApp;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
-import React from "react"
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import * as bootstrap from 'bootstrap'
-import App from './App'
+import App from './App.jsx'
+import './index.css'
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
